487b0bfbde79889e2b1f2d26690cdbd6153f67f6
alwe009 <63882279+alwe009@users.noreply.github.com>
2023-07-04 07:14:16 +0800

    working version

--- script.js
+++
@@ -1,7 +0,0 @@
-import Grid from "./Grid.js";
-
-const gameBoard = document.getElementById("game-board");
-
-const grid = new Grid(gameBoard);
-grid.randomEmptyCells().tile = new Tile(gameBoard);
-grid.randomEmptyCells().tile = new Tile(gameBoard);
